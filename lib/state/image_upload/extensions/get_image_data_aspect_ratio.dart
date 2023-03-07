a2c7e75cbd3a97e6edd3bdf36b5fe96f93ea0b3d
Inoh Pak <inohpak@inohs-mbp.lan>
2023-03-06 17:27:42 -0700

    posts/thumbnail

---
+++ lib/state/image_upload/extensions/get_image_data_aspect_ratio.dart
@@ -0,0 +1,10 @@
+import 'dart:typed_data' show Uint8List;
+import 'package:flutter/material.dart' as material show Image;
+import 'package:instagram_clone/state/image_upload/extensions/get_image_aspect_ratio.dart';
+
+extension GetImageDataAspectRatio on Uint8List {
+  Future<double> getAspectRatio() {
+    final image = material.Image.memory(this);
+    return image.getAspectRatio();
+  }
+}
---
+++ lib/state/image_upload/providers/image_upload_provider.dart
@@ -0,0 +1,7 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instagram_clone/state/image_upload/notifiers/image_upload_notifier.dart';
+import 'package:instagram_clone/state/image_upload/typedefs/is_loading.dart';
+
+final imageUploadProvider =
+    StateNotifierProvider<ImageUploadNotifier, IsLoading>(
+        (ref) => ImageUploadNotifier());
